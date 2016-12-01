bbe171c30430d1a8923205b154fe5213cc855037
yan <wyvernnot@gmail.com>
2016-12-01 15:51:32 +0800

    add timezone

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const StatsD = require('cloudinsight-sdk').StatsD;
 const schedule = require('node-schedule');
+const ZONES = require('moment-timezone/data/meta/latest.json').zones;
+const COUNTRIES = require('moment-timezone/data/meta/latest.json').countries;
+const tzs = Object.keys(ZONES);
+const moment = require('moment-timezone');
 
 const statsdClients = [
   new StatsD('127.0.0.1', 8251, ['label:a']),
@@ -40,6 +44,21 @@ schedule.scheduleJob('0 * * * * *', () => {
     statsd.gauge('base.gauge.60s.5', 5);
     statsd.incrementBy('base.counter.60s.5', 5);
   });
+});
+
+schedule.scheduleJob('30 * * * * *', () => {
+  tzs.forEach(function (tz) {
+    const now = moment().tz(tz);
+    const hour = parseInt(now.format('HH'));
+    const countries = ZONES[tz].countries;
+    statsdClients.forEach(statsd => {
+      const tags = [`timezone:${tz}`];
+      const countryTags = countries.map(c => `country:${COUNTRIES[c].name}`);
+      countries.forEach(function (country) {
+        statsd.gauge('base.hour', hour, tags.concat(countryTags));
+      })
+    });
+  })
 });
 
 console.log('started');
